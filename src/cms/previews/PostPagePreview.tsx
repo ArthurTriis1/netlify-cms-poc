ba6e579de46d87acbf6c16ec216b3240793822e6
Arthur Andrade <arthurfelandrade@gmail.com>
2022-03-21 22:55:52 -0300

    some configs
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- src/cms/previews/PostPagePreview.tsx
+++ src/cms/previews/PostPagePreview.tsx
@@ -5,7 +5,7 @@ import {marked} from 'marked'
 const PostPagePreview = ({ entry, getAsset }: any) => {
     const data = entry.getIn(['data']).toJS()
     const image = getAsset(data.thumb)
-    return (<PostTemplate thumb={image} title={data.title} html={marked.parse(data.body)} />)
+    return (<PostTemplate thumb={image} title={data.title} html={marked.parse(data.body || '')} />)
 }
 
 export default PostPagePreview
